622c2bdbfd1571672a0628e129b6201b3f502855
zhaojunfei <773152@qq.com>
2018-09-06 16:44:03 +0800

    解决单点登录的问题。

---
+++ src/main/java/org/jasig/cas/client/filter/AutoSetUserAdapterFilter.java
@@ -0,0 +1,104 @@
+package org.jasig.cas.client.filter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.UUID;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.jasig.cas.client.authentication.AttributePrincipal;
+import org.springframework.util.StringUtils;
+
+import com.greathiit.lab.entity.Customer;
+import com.greathiit.lab.service.CustomerService;
+
+
+
+public class AutoSetUserAdapterFilter implements Filter {
+
+	private static Logger logger = Logger.getLogger(AutoSetUserAdapterFilter.class);
+	
+	public FilterConfig config;
+	
+	private final static String LOGIN_SESSION_ID ="loginCustomer";
+	private final static String LOGIN_MESSAGE_INFO = "loginLaboratorys";
+	@Override
+	public void destroy() {
+
+	}
+
+	public final void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse,
+			final FilterChain filterChain) throws IOException, ServletException {
+		final HttpServletRequest request = (HttpServletRequest) servletRequest;
+		final HttpServletResponse response = (HttpServletResponse) servletResponse;
+		final HttpSession session = request.getSession(false);
+		 String adminRoleId = config.getInitParameter("adminRoleId");        // 登录登陆页面
+		String sessionid = String.valueOf(session.getAttribute(LOGIN_SESSION_ID));
+		//判断当前用户是否合法,合法直接放行
+		if(!StringUtils.isEmpty(sessionid)) {
+			filterChain.doFilter(request, response);
+		}
+		AttributePrincipal principal = (AttributePrincipal) request.getUserPrincipal();
+		java.util.Map<String, Object> attributes = principal.getAttributes();
+		String roleId = (String)attributes.get("roleId");
+		
+		roleId = roleId.replaceAll("[\\[\\]]", "");  
+		
+		String[] roleIds = roleId.split(",");
+		
+		String randPwd = randomPassword();
+		//如果角色里面包含99,那么认为是学生信息,不包含为教师信息,各业务系统插入各种业务系统的数据表就可以了可以保存用户信息，也可以直接放到session里面
+		if (!Arrays.asList(roleIds).contains("99")) {
+			if (attributes != null && attributes.size() > 0) {
+
+				String	loginname = attributes.get("login_name").toString();
+				String	no = attributes.get("no").toString();
+				
+				CustomerService customerService = (CustomerService)SpringContextHelper.getBean("customerService");
+				Customer customer = customerService.selectCustomeByCustId(Integer.valueOf(no));
+				if(StringUtils.isEmpty(customer)) {
+					 customer = new Customer();
+					 customer.setCustOid(getUUID() );
+					 customer.setCustId(Integer.valueOf(no));
+					 customer.setCustName(attributes.get("name").toString());
+					 customer.setCustPasswd("888888");
+					 customer.setCustImg("headImg/default.jpg");
+					 if(Arrays.asList(roleIds).contains(adminRoleId)) {
+						 customer.setCustRoot("2");
+					 }else {
+						 customer.setCustRoot("1");
+					 }
+					 
+					 customer.setCustDesc("初始化登录用户名:"+loginname);
+					 customerService.insertCustomer(customer);
+				}
+				session.setAttribute("loginCustomer", customerService.selectCustomeByCustId(customer.getCustId()));
+			}
+		}
+		
+		filterChain.doFilter(request, response);
+	}
+	
+	private String randomPassword(){
+		return String.valueOf((int)((Math.random()*9+1)*100000));
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		this.config = config;
+
+	}
+	public String getUUID() {
+		String s = UUID.randomUUID().toString();
+		return s.replace("-", "");
+	}
+}
